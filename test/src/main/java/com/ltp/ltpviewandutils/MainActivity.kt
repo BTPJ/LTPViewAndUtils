f8b5dca21f17b33a73d5d962ca8e01c0f3986a10
BTPJ <1069113473@qq.com>
2018-03-26 16:58:44 +0800

    添加LogUtil工具类
    Signed-off-by: BTPJ <1069113473@qq.com>

---
+++ test/src/main/java/com/ltp/ltpviewandutils/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ltp.ltpviewandutils
+
+import android.app.Activity
+import android.os.Bundle
+import com.ltp.viewandutils.utils.ToastUtil
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * LoadingDialog加载框的测试界面
+ *
+ * @author LTP 2018/3/2
+ */
+class MainActivity : Activity() {
+
+    var i = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_test.setOnClickListener {
+            //            LoadingDialog.Builder(this).create().show()
+            ToastUtil.showShort(this, "测试${i++}")
+        }
+    }
+}
