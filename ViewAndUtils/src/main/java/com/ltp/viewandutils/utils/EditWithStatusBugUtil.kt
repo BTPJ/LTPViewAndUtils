2bab0a60a3088c66341a80fb339b07c0247c2c78
BTPJ <1069113473@qq.com>
2018-07-02 16:02:01 +0800

    升级Gradle,修复EditWithStatusBugUtil类中适配华为虚拟键的Bug
    Signed-off-by: BTPJ <1069113473@qq.com>

--- ViewAndUtils/src/main/java/com/ltp/viewandutils/utils/EditWithStatusBugUtil.kt
+++ ViewAndUtils/src/main/java/com/ltp/viewandutils/utils/EditWithStatusBugUtil.kt
@@ -21,6 +21,14 @@ class EditWithStatusBugUtil(activity: Activity) {
     private var usableHeightPrevious: Int = 0
     private var frameLayoutParams: FrameLayout.LayoutParams
 
+    //为适应华为小米等手机键盘上方出现黑条或不适配
+    /** 获取setContentView本来view的高度 */
+    private var contentHeight: Int = 0
+    /** 只用获取一次 */
+    private var isFirst = true
+    /** 状态栏高度 */
+    private val statusBarHeight: Int = 0
+
     companion object {
 
         /**
@@ -34,40 +42,58 @@ class EditWithStatusBugUtil(activity: Activity) {
     }
 
     init {
+        // 1､找到Activity的最外层布局控件，它其实是一个DecorView,它所用的控件就是FrameLayout
         val content = activity.findViewById(android.R.id.content) as FrameLayout
+        // 2､获取到setContentView放进去的View
         mChildOfContent = content.getChildAt(0)
-        mChildOfContent.viewTreeObserver.addOnGlobalLayoutListener { possiblyResizeChildOfContent(activity) }
+        // 3､给Activity的xml布局设置View树监听，当布局有变化，如键盘弹出或收起时，都会回调此监听
+        mChildOfContent.viewTreeObserver.addOnGlobalLayoutListener {
+            // 4､软键盘弹起会使GlobalLayout发生变化
+            if (isFirst) {
+                //兼容华为等机型
+                contentHeight = mChildOfContent.height
+                isFirst = false
+            }
+            // 5､当前布局发生变化时，对Activity的xml布局进行重绘
+            possiblyResizeChildOfContent()
+        }
+        // 6､获取到Activity的xml布局的放置参数
         frameLayoutParams = mChildOfContent.layoutParams as FrameLayout.LayoutParams
     }
 
-    private fun possiblyResizeChildOfContent(activity: Activity) {
-        val usableHeightNow = computeUsableHeight(activity)
+    /**
+     * 获取界面可用高度，如果软键盘弹起后，Activity的xml布局可用高度需要减去键盘高度
+     */
+    private fun possiblyResizeChildOfContent() {
+        // 1､获取当前界面可用高度，键盘弹起后，当前界面可用布局会减少键盘的高度
+        val usableHeightNow = computeUsableHeight()
+        // 2､如果当前可用高度和原始值不一样
         if (usableHeightNow != usableHeightPrevious) {
+            // 3､获取Activity中xml中布局在当前界面显示的高度
             val usableHeightSansKeyboard = mChildOfContent.rootView.height
+            // 4､Activity中xml布局的高度-当前可用高度
             val heightDifference = usableHeightSansKeyboard - usableHeightNow
+            // 5､高度差大于屏幕1/4时，说明键盘弹出
             if (heightDifference > usableHeightSansKeyboard / 4) {
-                // keyboard probably just became visible
-                frameLayoutParams.height = usableHeightSansKeyboard - heightDifference
+                // 6､键盘弹出了，Activity的xml布局高度应当减去键盘高度
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference + statusBarHeight
+                } else {
+                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference
+                }
             } else {
-                // keyboard probably just became hidden
                 frameLayoutParams.height = usableHeightSansKeyboard
             }
+            // 7､ 重绘Activity的xml布局
             mChildOfContent.requestLayout()
             usableHeightPrevious = usableHeightNow
         }
     }
 
-    private fun computeUsableHeight(activity: Activity): Int {
-        val frame = Rect()
-        activity.window.decorView.getWindowVisibleDisplayFrame(frame)
-        val statusBarHeight = frame.top
+    private fun computeUsableHeight(): Int {
         val r = Rect()
         mChildOfContent.getWindowVisibleDisplayFrame(r)
-
-        //这个判断是为了解决19之后的版本在弹出软键盘时，键盘和推上去的布局（adjustResize）之间有黑色区域的问题
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            return (r.bottom - r.top) + statusBarHeight
-        }
+        // 全屏模式下：直接返回r.bottom，r.top其实是状态栏的高度
         return (r.bottom - r.top)
     }
 }
