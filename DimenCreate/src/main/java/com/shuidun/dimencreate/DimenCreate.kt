779cfd0f9b1ed915ecbe0b0a724114d3e7b5f3cc
BTPJ <1069113473@qq.com>
2018-10-30 14:11:20 +0800

    添加生成smallest-values适配工具lib
    Signed-off-by: BTPJ <1069113473@qq.com>

--- DimenCreate/src/main/java/com/shuidun/dimencreate/DimenCreate.kt
+++ DimenCreate/src/main/java/com/shuidun/dimencreate/DimenCreate.kt
@@ -3,7 +3,7 @@ package com.shuidun.dimencreate
 import java.io.File
 
 /**
- * dimens文件生成主类
+ * dimens文件生成主类 https://github.com/ladingwu/dimens_sw
  *
  * @author LTP  2018/10/29
  */
@@ -19,7 +19,7 @@ object DimenCreate {
     fun main(args: Array<String>) {
         val smallest = if (DESIGN_WIDTH > DESIGN_HEIGHT) DESIGN_HEIGHT else DESIGN_WIDTH  //     求得最小宽度
         // 要生成的dimens文件的最小宽度（Smallest-width）限定符
-        val values = intArrayOf(300, 310, 320, 330, 330, 340, 350, 360, 370, 380, 390, 400, 411, 420, 430, 440, 450, 480)
+        val values = intArrayOf(300, 310, 320, 330, 330, 340, 350, 360, 370, 384, 390, 400, 411, 420, 432, 440, 450, 480, 533, 600)
         for (value in values) {
             val directory = File("MyValues")//为了方便，设定为当前文件夹，dimens文件将会生成项目所在文件夹中，用户可自行更改
             DimenCreateManager.makeAll(smallest, value, directory.absolutePath)
